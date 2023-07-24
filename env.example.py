a8f3f67b5095b20a7bfd831f3ee9011dff8fe0ae
proffapt <proffapt@pm.me>
2023-07-24 15:47:52 +0530

    adding env.py example file/template

---
+++ env.example.py
@@ -0,0 +1,15 @@
+# ERP Credentials
+ROLL_NUMBER = "XXYYXXXXX"
+PASSWORD = "**********"
+SECURITY_QUESTIONS_ANSWERS = {
+    "Q1" : "A1",
+    "Q2" : "A2",
+    "Q3" : "A3",
+}
+
+# EMAIL CREDENTIALS
+FROM_EMAIL = "abc@gmail.com"
+FROM_EMAIL_PASS = "**********"
+
+# OTHER PARAMETERS
+TO_EMAIL = "xyz@googlegroup.com"
