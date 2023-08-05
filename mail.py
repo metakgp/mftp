974ce6741c411caa3b42139e81ac3c4874b80877
proffapt <proffapt@pm.me>
2023-08-05 12:30:02 +0530

    feat: Better formatting for mail body

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+import re
 import smtplib, ssl
 from email import encoders
 from bs4 import BeautifulSoup as bs
@@ -10,9 +11,9 @@ from endpoints import NOTICE_CONTENT_URL, ATTACHMENT_URL
 def format(notices, session): 
     mails = []
     for notice in notices:
-        message = MIMEMultipart()
+        message = MIMEMultipart('alternative')
         message["Subject"] = f"{notice['Subject']} - {notice['Company']}"
-        message["From"] = FROM_EMAIL
+        message["From"] = 'MFTP <' + FROM_EMAIL + '>'
         message["To"] = TO_EMAIL
 
         uid = notice['UID'].split('_')
@@ -21,9 +22,27 @@ def format(notices, session):
         attachment = uid[2]
         
         body = parseBody(session, year, id_)
+        # Hyperlinking any link with <click here> to reduce link clutter
+        body = re.sub(r"(https?://[^\s]+)", r'<a href="\1">click here</a>', body)
+        # Decent enough layout for the mail
+        body = f"""
+        <html>
+            <body>
+                <div style="font-family: Arial, sans-serif; width: 90%; margin: 0 auto; border: 1px solid #333; padding: 20px; margin-bottom: 20px; border-radius: 10px; box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);">
+                    <div style="margin-bottom: 20px;">
+                        {body}
+                    </div>
+                    <div style="text-align: right; font-style: italic;">
+                        ({notice['Time']})
+                    </div>
+                </div>
+            </body>
+        </html>
+        """
+        
         message.attach(MIMEText(body, "html"))
         
-        if attachment:
+        if eval(attachment):
             file = MIMEBase('application', 'octet-stream')
             attachment = parseAttachment(session, year, id_)
             file.set_payload(attachment)
