264240a8bb5ba5a494038f09826ed4fe04f7ca4f
proffapt <proffapt@pm.me>
2023-09-02 02:30:47 +0530

    feat: MFTP as a service for *nix

---
+++ service/status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+WHITE=$(tput setaf 7)
+
+KERNEL=$(uname -s)
+if [[ "$KERNEL" == "Darwin" ]]; then
+  mftpID=$(ps -a | grep -i "mftp.py" | grep -v "grep" | head -n 1 | awk '{print $1}')
+else
+  mftpID=$(ps -aux | grep -i "mftp.py" | grep -v "grep" | head -n 1 | awk '{print $2}')
+fi
+
+if [ -n "$mftpID" ]; then
+	echo -e "${GREEN}  [ACTIVE]${YELLOW} : PID : ${BLUE}${mftpID}${WHITE}"
+else
+	echo -e "${RED}  [INACTIVE]${WHITE}"
+fi
