1002fc180e1a12a706316b125bac660d821ab3a9
proffapt <proffapt@pm.me>
2023-09-02 02:47:18 +0530

    feat(alias configuration): Added support for ZSH

--- service/setup-service.sh
+++ service/setup-service.sh
@@ -10,8 +10,15 @@ echo "export MFTPD=$(dirname $(pwd))" >> mftp-service-aliases
 
 cat mftp-service-aliases
 
-ALIAS_CONFIGURED=$(grep -q 'mftp-service-aliases' ~/.bashrc && echo true || echo false)
+if [[ "$(basename $SHELL)" == "bash" ]]; then
+  ALIAS_CONFIGURED=$(grep -q 'mftp-service-aliases' ~/.bashrc && echo true || echo false)
+  SHELL_RC=~/.bashrc
+elif [[ "$(basename $SHELL)" == "zsh" ]]; then
+  ALIAS_CONFIGURED=$(grep -q 'mftp-service-aliases' ~/.zshrc && echo true || echo false)
+  SHELL_RC=~/.zshrc
+fi
+
 if [ "$ALIAS_CONFIGURED" == "false" ]; then
-	echo "source $(pwd)/mftp-service-aliases" >> ~/.bashrc
-	source ~/.bashrc
+  echo "source $(pwd)/mftp-service-aliases" >> "$SHELL_RC"
+  source "$SHELL_RC"
 fi
