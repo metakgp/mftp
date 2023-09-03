334f7ac1e7f7e2ab62ad06f5b34a5939d3926e76
proffapt <proffapt@pm.me>
2023-09-03 13:54:40 +0530

    fix: Removing setsid for service

--- service/systemd/mftp-startup-service.sh
+++ service/systemd/mftp-startup-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd MFTPD || exit 1
-setsid python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1 &
+python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1
 echo "[+] Successfully started MFTP server!"
 cd - >/dev/null || exit 1
