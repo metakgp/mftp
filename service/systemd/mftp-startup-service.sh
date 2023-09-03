db05c3ceee2c24c7aab6b076442f3bd74458e53b
proffapt <proffapt@pm.me>
2023-09-03 16:08:13 +0530

    feat: Better logging on state of execution

--- service/systemd/mftp-startup-service.sh
+++ service/systemd/mftp-startup-service.sh
@@ -8,6 +8,7 @@ done
 # Now starts the script
 cd MFTPD || exit 1
 export PYTHONPATH=$PYTHONPATH:$(python3 -m site --user-site)
-python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1
-echo "[+] Successfully started MFTP server!"
+python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1 &&\
+  echo "[+] Successfully started MFTP server!" ||\
+  echo "[ERROR] Failed to start MFTP server"
 cd - >/dev/null || exit 1
