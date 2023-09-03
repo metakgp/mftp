de8b7b159af50d5159cda01a68b12ab2942d8daf
proffapt <proffapt@pm.me>
2023-09-03 11:54:50 +0530

    feat(systemd): Updated logic for configuring startup service

--- service/configure-service.sh
+++ service/configure-service.sh
@@ -20,3 +20,27 @@ if [ "$ALIAS_CONFIGURED" == "false" ]; then
   echo "source $(pwd)/mftp-service-aliases" >> "$SHELL_RC"
   source "$SHELL_RC"
 fi
+
+KERNEL=$(uname -s)
+if [[ "$KERNEL" == "Linux" ]]; then
+  sed -i "s/MFTPD/${MFTPD}/g" systemd/mftp.service
+  sed -i "s/MFTPD/${MFTPD}/g" systemd/mftp-startup-service.sh
+
+  if [ ! -f /etc/systemd/system/mftp.service ]; then
+    echo -e "${GREEN}[+] ${BLUE}Creating MFTP startup service${WHITE}"
+    chmod 644 systemd/mftp.service
+    sudo cp systemd/mftp.service /etc/systemd/system/
+    sudo chmod 777 /etc/systemd/system/mftp.service
+    sudo systemctl daemon-reload
+  else
+    echo -e "${YELLOW}[-] ${BLUE}MFTP startup service already created${WHITE}"
+  fi
+
+  if [ "$(systemctl is-enabled mftp)" == "disabled" ]; then
+    echo -e "${GREEN}[+] ${BLUE}Enabling MFTP startup service${WHITE}"
+    sudo systemctl enable mftp
+  else
+    echo -e "${YELLOW}[-] ${BLUE}MFTP startup service already enabled${WHITE}"
+  fi
+fi
+
