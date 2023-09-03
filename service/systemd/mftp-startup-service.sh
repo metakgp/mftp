a8b4f441e049608f25cbb7045d97a84a565d23ea
proffapt <proffapt@pm.me>
2023-09-03 14:06:22 +0530

    fix: Site packages weren't configured

--- service/systemd/mftp-startup-service.sh
+++ service/systemd/mftp-startup-service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd MFTPD || exit 1
+export PYTHONPATH=$PYTHONPATH:$(python3 -m site --user-site)
 python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1
 echo "[+] Successfully started MFTP server!"
 cd - >/dev/null || exit 1
