6b62105d8f2dfda3abe509599451c88d25c01cec
proffapt <proffapt@pm.me>
2023-09-03 15:16:55 +0530

    feat: Implemented network check before actual execution

--- service/systemd/mftp-startup-service.sh
+++ service/systemd/mftp-startup-service.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Sleeping till the network is not available
+while ! ip route | grep -q default; do
+  sleep 1
+done
+
+# Now starts the script
 cd MFTPD || exit 1
 export PYTHONPATH=$PYTHONPATH:$(python3 -m site --user-site)
 python3 mftp.py --${1:-gmail-api} >>logs.txt 2>&1
