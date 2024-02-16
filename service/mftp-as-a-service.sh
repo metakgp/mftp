22b46b72b16b49e0c36a30a5e10f07eb5924d93c
proffapt <proffapt@pm.me>
2024-02-16 17:55:58 +0530

    feat: implemented mftp doctor as a cronjob

--- service/mftp-as-a-service.sh
+++ service/mftp-as-a-service.sh
@@ -7,26 +7,33 @@ WHITE=$(tput setaf 7)
 
 usage() {
   cat <<-EOF 
-Usage: ${0##*/} [OPTIONS]
+Usage: ${0##*/} [COMMAND] [OPTIONS]
 
-Options:
-  -h, --help               Display this help and exit
-  logs [OPTIONS]           Display last 25 lines of log file
+Command:
+  -h, --help                   Display this help and exit
+  logs [OPTIONS]               Display last 25 lines of log file
     Options:
-      clear                 Clear the log file
-      NUM                   Display last NUM lines of log file
-  disable                  Disable mftp service  
-  enable                   Enable mftp service
-  status                   Check status of mftp service
-  restart                  Restart mftp service
-  stop                     Stop mftp service  
-  start                    Start mftp service
-  cronjob [OPTIONS]        Use mftp as a cronjob
+      clear                    Clear the log file
+      NUM                      Display last NUM lines of log file
+  disable                      Disable mftp service  
+  enable                       Enable mftp service
+  status                       Check status of mftp service
+  restart                      Restart mftp service
+  stop                         Stop mftp service  
+  start                        Start mftp service
+
+  cronjob [OPTIONS]            Use mftp as a cronjob
     Options:
       enable [NUM]             Enable mftp cronjob after every NUM minutes (default is 2 minutes)
       disable                  Disable mftp cronjob
       status                   Check status of mftp cronjob
-      
+
+  doctor [OPTIONS]             Use mftp doctor as a cronjob
+    Options:
+      enable [NUM]             Enable doctor cronjob after every NUM minutes (default is 2 minutes)
+      disable                  Disable doctor cronjob
+      status                   Check status of doctor cronjob
+
 EOF
 }
 
@@ -84,6 +91,44 @@ case "$1" in
 "start")
   sudo systemctl start mftp
   ;;
+"doctor")
+  # Getting the status of configuration of mftp-doctor cronjob
+  if ! crontab -l | grep -q "mftp-doctor.py"; then
+    doctor_cron_enabled="False"
+  else
+    doctor_cron_enabled="True"
+  fi
+
+  case "$2" in
+  "enable")
+    if [[ "$3" =~ ^[0-9]+$ ]] || [[ -z "$3" ]]; then
+      if [[ "$doctor_cron_enabled" == "False" ]]; then
+        enable_cronjob "${3:-2}" "mftp-doctor"
+        echo -e "${GREEN}[+] ${WHITE}Doctor Cron configured!"
+      else
+        echo -e "${YELLOW}[~] ${WHITE}Doctor Cron already configured"
+      fi
+    else
+      echo -e "${RED}[ERROR] ${WHITE} Invalid argument for \`${YELLOW}mftp cronjob enable${WHITE}\`"
+    fi
+    ;;
+  "disable")
+    if [[ "$doctor_cron_enabled" == "True" ]]; then
+      disable_cronjob "mftp-doctor"
+      echo -e "${GREEN}[+] ${WHITE}Doctor Cron disabled!"
+    else
+      echo -e "${YELLOW}[~] ${WHITE}Doctor Cron already disabled"
+    fi
+    ;;
+  "status")
+    if [[ "$doctor_cron_enabled" == "True" ]]; then
+      echo -e "${GREEN}[+]${WHITE} Doctor Cron is configured!"
+    else
+      echo -e "${RED}[-]${WHITE} Doctor Cron is not configured"
+    fi 
+    ;;
+  esac
+  ;;
 "cronjob")
   # Getting the status of configuration of mftp cronjob
   if ! crontab -l | grep -q "mftp.py"; then
@@ -106,7 +151,12 @@ case "$1" in
     fi
     ;;
   "disable")
-    disable_cronjob "mftp"
+    if [[ "$cron_enabled" == "True" ]]; then
+      disable_cronjob "mftp"
+      echo -e "${GREEN}[+] ${WHITE}Doctor Cron disabled!"
+    else
+      echo -e "${YELLOW}[~] ${WHITE}Doctor Cron already disabled"
+    fi
     ;;
   "status")
     if [[ "$cron_enabled" == "True" ]]; then
