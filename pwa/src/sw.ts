326ff27e6b08b69db9b49596976d520c0f04ac7d
Chirag Ghosh <cghosh828049@gmail.com>
2024-06-05 21:16:07 +0530

    feat: remove unnecessary files. use sass. use prettier
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- pwa/src/sw.ts
+++ pwa/src/sw.ts
@@ -1,26 +1,28 @@
 /// <reference lib="webworker" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import {
+    cleanupOutdatedCaches,
+    createHandlerBoundToURL,
+    precacheAndRoute,
+} from "workbox-precaching";
+import { clientsClaim } from "workbox-core";
+import { NavigationRoute, registerRoute } from "workbox-routing";
 
-declare let self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope;
 
 // self.__WB_MANIFEST is the default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST);
 
 // clean old assets
-cleanupOutdatedCaches()
+cleanupOutdatedCaches();
 
-let allowlist: RegExp[] | undefined
+let allowlist: RegExp[] | undefined;
 // in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+if (import.meta.env.DEV) allowlist = [/^\/$/];
 
 // to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(
+    new NavigationRoute(createHandlerBoundToURL("index.html"), { allowlist }),
+);
 
-self.skipWaiting()
-clientsClaim()
+self.skipWaiting();
+clientsClaim();
