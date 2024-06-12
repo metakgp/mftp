a55f5293bd301b69931ed3ec9d3e1c1dc845081b
Dhanvith Nayak <dhanpran3@gmail.com>
2024-06-12 16:43:56 +0530

    feat(ntfy): notification tags and priority

--- mftp/ntfy.py
+++ mftp/ntfy.py
@@ -2,33 +2,44 @@ import requests
 from bs4 import BeautifulSoup as bs
 import logging
 from notice import update_lsni
-from endpoints import NOTICE_CONTENT_URL
+from endpoints import NOTICE_CONTENT_URL, ATTACHMENT_URL
 
 def format_notice(notices, session):
   notifications=[]
 
   for notice in reversed(notices):
     id_, year = notice['UID'].split('_')
+
     try:
       data = parseBody(session, year, id_)
     except Exception as e:
-      logging.error(f" Failed to parse notification body ~ {str(e)}")
+      logging.error(f"Failed to parse notification body ~ {str(e)}")
+
+    if notice['Subject'] == 'Urgent':
+      priority="5"
+    else:
+      priority="4"
 
     notifications.append(
-      {"Title": f"{notice['Type']} : {notice['Company']} {notice['Subject']}", 
-       "Data": data})
+      {"Title":  f"#{id_} | {notice['Type']} | {notice['Subject']} | {notice['Company']}",
+        "Data": data,
+        "Tags": f"{notice['Type']}, {notice['Subject']}, {notice['Company']}",
+        "Priority": priority})
   
   return notifications
 
 def send(notifications, lsnif, notices):
   for i, notification in enumerate(notifications, 1): 
     try:
-      requests.post("http://172.18.0.2:8000/mftp", 
+      requests.post("http://172.18.0.2:8000/mftp",
         data=notification["Data"],
         headers={ 
           "Title": notification["Title"],
+          "Tags": notification["Tags"],
+          "Priority": notification["Priority"],
           "Markdown": "yes"})
       update_lsni(lsnif, notices, i)
+      print(f'Sent notification {notification["Title"]}')
     except Exception as e:
       logging.error(f"Failed to send notification to ntfy server ~ {str(e)}")  
 
