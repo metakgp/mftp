d9d8608e485dd73388c7b04d6c2248dfc2fededb
Dhanvith Nayak <dhanpran3@gmail.com>
2024-06-13 17:03:20 +0530

    refactor(ntfy): improved logging

--- mftp/ntfy.py
+++ mftp/ntfy.py
@@ -6,79 +6,86 @@ from notice import update_lsni, get_latest_index
 from endpoints import NOTICE_CONTENT_URL
 
 def format_notice(notices, session):
-  notifications=[]
+    if notices: print('[FORMATTING NOTIFICATIONS]', flush=True)
 
-  for notice in reversed(notices):
-    id_, year = notice['UID'].split('_')
+    notifications=[]
 
-    try:
-      data = parseBody(notice, session, year, id_)
-      body, links = parseLinks(data, id_)
-    except Exception as e:
-      logging.error(f"Failed to parse notification body ~ {str(e)}")
+    for notice in reversed(notices):
+        id_, year = notice['UID'].split('_')
 
-    if notice['Subject'] == 'Urgent':
-      priority="5"
-    else:
-      priority="4"
+        try:
+            data = parseBody(notice, session, year, id_)
+            body, links = parseLinks(data, id_)
+        except Exception as e:
+            logging.error(f" Failed to parse notification body ~ {str(e)}")
 
-    notifications.append(
-      {"Title":  f"#{id_} | {notice['Type']} | {notice['Subject']} | {notice['Company']}",
-        "Body": body,
-        "Tags": f"{notice['Type']}, {notice['Subject']}, {notice['Company']}",
-        "Priority": priority,
-        "Links": links})
+        match notice['Subject']:
+            case 'Urgent':
+                priority="5"
+            case _:
+                priority="3"
+
+        notifications.append(
+            {"Title":  f"#{id_} | {notice['Type']} | {notice['Subject']} | {notice['Company']}",
+                "Body": body,
+                "Tags": f"{notice['Type']}, {notice['Subject']}, {notice['Company']}",
+                "Priority": priority,
+                "Links": links})
   
-  return notifications
+    return notifications
 
 def send(notifications, lsnif, notices):
-  for i, notification in enumerate(notifications, 1): 
-    if has_idx_mutated(lsnif, notices, i): break
+    if notifications:
+        print("[SENDING NOTIFICATIONS]", flush=True)
 
-    try:
-      requests.post("http://172.18.0.2:8000/mftp",
-        data=notification["Body"],
-        headers={ 
-          "Title": notification["Title"],
-          "Tags": notification["Tags"],
-          "Priority": notification["Priority"],
-          "Icon": "https://miro.medium.com/v2/resize:fit:600/1*O94LHxqfD_JGogOKyuBFgA.jpeg",
-          "Action": notification["Links"]})
-      update_lsni(lsnif, notices, i)
-    except Exception as e:
-      logging.error(f"Failed to send notification to ntfy server ~ {str(e)}")  
+        for i, notification in enumerate(notifications, 1):
+            if has_idx_mutated(lsnif, notices, i): break
+
+            try:
+                requests.post("http://172.18.0.2:8000/mftp",
+                    data=notification["Body"],
+                    headers={
+                        "Title": notification["Title"],
+                        "Tags": notification["Tags"],
+                        "Priority": notification["Priority"],
+                        "Icon": "https://miro.medium.com/v2/resize:fit:600/1*O94LHxqfD_JGogOKyuBFgA.jpeg",
+                        "Action": notification["Links"]})
+                update_lsni(lsnif, notices, i)
+                logging.info(f" [NOTIFICATION SENT] ~ {notification['Title']}")
+
+            except Exception as e:
+                logging.error(f" Failed to send notification: {notification['Title']} ~ {str(e)}")
 
 def parseBody(notice, session, year, id_):
-  content = session.get(NOTICE_CONTENT_URL.format(year, id_))
-  content_html = bs(content.text, 'html.parser')
-  content_html_div = bs.find_all(content_html, 'div', {'id': 'printableArea'})[0]
+    content = session.get(NOTICE_CONTENT_URL.format(year, id_))
+    content_html = bs(content.text, 'html.parser')
+    content_html_div = bs.find_all(content_html, 'div', {'id': 'printableArea'})[0]
 
-  body = ''
-  for br in content_html_div.find_all('br'):
-    body = body + br.next_sibling.strip() + '\n'
+    body = ''
+    for br in content_html_div.find_all('br'):
+        body = body + br.next_sibling.strip() + '\n'
 
-  body = body + notice['Time']
+    body = body + notice['Time']
 
-  return body
+    return body
 
 def parseLinks(data, id_):
-  body = data
-  links = re.findall(r'(https?://[^\s]+)', data)
-  links = list(set(result.lower() for result in links))
-  action_template = "view, Link {}, {}"
-  actions = ""
+    body = data
+    links = re.findall(r'(https?://[^\s]+)', data)
+    links = list(set(result.lower() for result in links))
+    action_template = "view, Link {}, {}"
+    actions = ""
 
-  for i, link in enumerate(links, 1):
-    if i == 4:
-      break
+    for i, link in enumerate(links, 1):
+        if i == 4: break
 
-    if i > 1:
-      actions = actions + "; "
-    body = body.replace(link, f'<Link {i}>')
-    template = action_template
-    actions = actions + template.format(i, link)
+        if i > 1:
+            actions = actions + "; "
+        body = body.replace(link, f'<LINK {i}>')
+        template = action_template
+        actions = actions + template.format(i, link)
 
-  return body, actions
+    return body, actions
 
 def has_idx_mutated(lsnif, notices, i):
     lidx_from_file = get_latest_index(lsnif) # Latest Index from File
