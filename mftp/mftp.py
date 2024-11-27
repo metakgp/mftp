d206ab49825d8da869f2728433aec35b4dcb8cbb
proffapt <proffapt@pm.me>
2024-11-27 23:20:21 +0530

    fix: parse companies every hour properly

--- mftp/mftp.py
+++ mftp/mftp.py
@@ -8,8 +8,8 @@ import company
 
 import requests
 import argparse
+from datetime import datetime
 import iitkgp_erp_login.erp as erp
-from datetime import datetime, timedelta
 
 headers = {
   'timeout': '20',
@@ -24,10 +24,6 @@ parser.add_argument('--ntfy', action="store_true", help='Use NTFY to broadcast n
 parser.add_argument('--cron', action="store_true", help='Act as cronjob, bypass the continuous loop', required=False)
 args = parser.parse_args()
 
-# Initialize the last execution time for companies email logic
-# Set it to run immediately on the first iteration
-last_companies_email_time = datetime.now() - timedelta(hours=1)
-
 while True:
   now = datetime.now()
   print(f"================ <<: {now.strftime('%H:%M:%S %d-%m-%Y')} :>> ================", flush=True)
@@ -36,12 +32,11 @@ while True:
   _, ssoToken = erp.login(headers, session, ERPCREDS=env, OTP_CHECK_INTERVAL=2, LOGGING=True, SESSION_STORAGE_FILE='.session')
 
   if args.gmail_api or args.smtp:
-    if now - last_companies_email_time >= timedelta(hours=1):
+    if now.minute == 0:
       companies = company.fetch(session, headers, ssoToken)
       open_not_applied_companies = company.filter(companies, "OPEN_N")
       companies_update_mail = mail.format_companies(session.cookies.get('ssoToken'), open_not_applied_companies)
       mail.send_companies(companies_update_mail, args.gmail_api, args.smtp)
-      last_companies_email_time = now
 
   notice_db = db.NoticeDB(config={
     'uri': env.MONGO_URI,
